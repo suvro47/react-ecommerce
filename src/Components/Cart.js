ffbba013194dce4d660463380c275e8fe2753080
suvro47 <suvro047@gmail.com>
2021-09-15 17:51:54 +0600

    saved

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 const customStyles = {
   content: {
     top: "80px",
-    left: "2px",
+    left: "39%",
     right: "0%",
     bottom: "auto",
     width: "57%",
@@ -18,31 +18,7 @@ function Cart() {
   const { info, isModalOpen, setIsModalOpen } = useContext(Context);
 
   let cost = 0;
-
-  if (info.length === 0) {
-    return (
-      <Modal
-        isOpen={isModalOpen}
-        ariaHideApp={false}
-        onRequestClose={() => setIsModalOpen(false)}
-        style={customStyles}
-        htmlOpenClassName="overflow-hidden"
-      >
-        <button
-          className="float-right -mt-4 text-3xl font-semibold"
-          onClick={(e) => {
-            e.preventDefault();
-            setIsModalOpen(false);
-          }}
-        >
-          &times;
-        </button>
-        <div className="flex flex-row justify-between w-1/2 p-2 m-auto text-xl font-black text-red-900">
-          <h1> Opps !! You haven't selected any product yet :( </h1>
-        </div>
-      </Modal>
-    );
-  }
+  let flag = false;
 
   return (
     <Modal
@@ -61,20 +37,34 @@ function Cart() {
       >
         &times;
       </button>
-      <div className="m-10">
-        {info.map((c, index) => {
-          cost += c.price * c.count;
-          return <HorizentalCard key={index} props={c} />;
-        })}
-      </div>
 
-      <div className="flex flex-row justify-between w-1/2 p-2 m-auto ">
-        <div className="flex justify-center w-auto px-2 py-1 text-base text-white bg-red-700 rounded shadow-2xl justipy-1">
-          Total Cost : {cost.toFixed(2)} $
+      <div className="main-content">
+        <div className="m-10">
+          {info.map((c, index) => {
+            cost += c.price * c.count;
+            if (cost > 0) flag = true;
+            return <HorizentalCard key={index} props={c} />;
+          })}
         </div>
-        <button className="px-2 py-1 text-base text-white bg-green-700 rounded shadow-2xl">
-          <Link to="/purchese"> Request To Process</Link>
-        </button>
+
+        {(cost === 0.0 ? true : false) && (
+          <div className="warning">
+            <div className="flex flex-row justify-between w-3/4 m-auto mt-20 text-xl font-black text-red-900">
+              <h1> Opps !! You haven't selected any product yet :( </h1>
+            </div>
+          </div>
+        )}
+
+        {(cost !== 0.0 ? true : false) && (
+          <div className="flex flex-row justify-between w-1/2 p-2 m-auto ">
+            <div className="flex justify-center w-auto px-2 py-1 text-base text-white bg-red-700 rounded shadow-2xl justipy-1">
+              Total Cost : {cost.toFixed(2)} $
+            </div>
+            <button className="px-2 py-1 text-base text-white bg-green-700 rounded shadow-2xl">
+              <Link to="/purchese"> Request To Process</Link>
+            </button>
+          </div>
+        )}
       </div>
     </Modal>
   );
