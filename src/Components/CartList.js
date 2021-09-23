faa2b252cb5c9479d328d7a85fd113b8d81b7b6c
suvro47 <suvro047@gmail.com>
2021-09-23 15:21:00 +0600

    code-refactred

--- src/Components/CartList.js
+++ src/Components/CartList.js
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { Context } from "../GlobalContexts/ContextHandler";
 import Modal from "react-modal";
-import Item from "./Item";
+import Cart from "./Cart";
 import { Link } from "react-router-dom";
 
 const customStyles = {
@@ -14,7 +14,7 @@ const customStyles = {
     height: "80%",
   },
 };
-function Preview() {
+function CartList() {
   const { info, isModalOpen, setIsModalOpen } = useContext(Context);
 
   let cost = 0;
@@ -25,10 +25,10 @@ function Preview() {
       ariaHideApp={false}
       onRequestClose={() => setIsModalOpen(false)}
       style={customStyles}
-      htmlOpenClassName="overflow-hidden"
+      htmlOpenClassName='overflow-hidden'
     >
       <button
-        className="-mt-4 text-3xl font-semibold"
+        className='-mt-4 text-3xl font-semibold'
         onClick={(e) => {
           e.preventDefault();
           setIsModalOpen(false);
@@ -37,34 +37,34 @@ function Preview() {
         &times;
       </button>
 
-      <div className="main-content">
-        <div className="m-10">
+      <div className='main-content'>
+        <div className='m-10'>
           {info.map((c, index) => {
             cost += c.price * c.count;
-            return <Item key={index} props={c} />;
+            return <Cart key={index} props={c} />;
           })}
         </div>
 
         {cost === 0 && (
-          <div className="warning">
-            <div className="flex flex-row justify-between w-3/4 m-auto mt-20 text-xl font-black text-red-900">
+          <div className='warning'>
+            <div className='flex flex-row justify-between w-3/4 m-auto mt-20 text-xl font-black text-red-900'>
               <h1> Opps !! You haven't selected any product yet :( </h1>
             </div>
           </div>
         )}
 
         {cost !== 0 && (
-          <div className="flex flex-row justify-center w-1/2 p-2 m-auto">
+          <div className='flex flex-row justify-center w-1/2 p-2 m-auto'>
             <button
-              className="px-2 py-1 mx-8 text-base text-white bg-red-700 rounded shadow-2xl"
+              className='px-2 py-1 mx-8 text-base text-white bg-red-700 rounded shadow-2xl'
               onClick={(e) => {
                 e.preventDefault();
               }}
             >
               Clear Cart
             </button>
-            <button className="px-2 py-1 text-base text-white bg-green-700 rounded shadow-2xl">
-              <Link to="/checkout"> Checkout </Link>
+            <button className='px-2 py-1 text-base text-white bg-green-700 rounded shadow-2xl'>
+              <Link to='/checkout'> Checkout </Link>
             </button>
           </div>
         )}
@@ -73,4 +73,4 @@ function Preview() {
   );
 }
 
-export default Preview;
+export default CartList;
--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -2,17 +2,16 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Navbar from "./Navbar";
 import Product from "./Product";
-import Preview from "./Preview";
+import CartList from "./CartList";
 import Loader from "react-loader-spinner";
 
 //import image from "../assets/image.jpg";
 
-function Home() {
+function ProductList() {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    //
     axios
       .get("https://fakestoreapi.com/products")
       .then((res) => {
@@ -27,30 +26,30 @@ function Home() {
 
   return (
     <div
-      className="h-auto font-sans font-medium bg-contain bg-gradient-to-r from-gray-100 to-gray-200" //style={{ backgroundImage: `url(${image})` }}
+      className='h-auto font-sans font-medium bg-contain bg-gradient-to-r from-gray-100 to-gray-200' //style={{ backgroundImage: `url(${image})` }}
     >
-      <section className="sticky top-0">
+      <section className='sticky top-0'>
         <Navbar />
       </section>
 
       {!isLoading ? (
         <div>
-          <section className="flex flex-wrap justify-around pb-10">
+          <section className='flex flex-wrap justify-around pb-10'>
             {products.map((p, index) => (
               <Product key={index} props={p} />
             ))}
           </section>
           <section>
-            <Preview />
+            <CartList />
           </section>
         </div>
       ) : (
-        <div className="w-20 h-screen m-auto my-40">
-          <Loader type="Bars" color="#00BFFF" height={80} width={100} />
+        <div className='w-20 h-screen m-auto my-40'>
+          <Loader type='Bars' color='#00BFFF' height={80} width={100} />
         </div>
       )}
     </div>
   );
 }
 
-export default Home;
+export default ProductList;
